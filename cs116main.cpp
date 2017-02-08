e24297824fc9b838e5879de5cde5e5ab906fe91b
Mohammed Abbas <abbas.a.mohammed95@gmail.com>
2017-02-08 10:17:08 -0800

    Update cs116main.cpp

--- cs116main.cpp
+++ cs116main.cpp
@@ -1,104 +1,120 @@
+// ConversionCalculator.cpp : Defines the entry point for the console application.
+//#include <stdafx.h>
 #include <iostream>
 #include <string>
-#include <iomanip>
 #include <ctime>  // used for time function
+using namespace std;
 
-void printMeFirst(std::string name, std::string courseInfo, std::string version) 
+void printMeFirst(string name, string courseInfo, string version)
 {
-    std::cout << "Program written by: " << name << std::endl; // put your name here
-    std::cout << "Course info: " << courseInfo << std::endl; //course info goes here
-    std::cout << "Version Number " << version << std::endl;
-    
-    time_t now = time(0);  // current date/time based on current system
-    char* dt = ctime(&now); // convert now to string for
-    
-    std::cout << " Date: " << dt << std::endl; //prints the date
+	cout << "Program written by: " << name << endl; // put your name here
+	cout << "Course info: " << courseInfo << endl; //course info goes here
+	cout << "Version Number " << version << endl;
+
+	time_t now = time(0);  // current date/time based on current system
+	char* dt = ctime(&now); // convert now to string for
+
+	cout << " Date: " << dt << endl; //prints the date
 }
 //function to organize all of the options listed on menu
-void options(std::string opt1, std::string opt2, std::string opt3, std::string opt4)
+void options(string opt1, string opt2, string opt3, string opt4)
 {
-    std::cout << opt1 << std::endl; //option 1
-    std::cout << opt2 << std::endl; //option 2
-    std::cout << opt3 << std::endl; //option 3
-    std::cout << opt4 << std::endl; //option 4
+	cout << opt1 << endl; //option 1
+	cout << opt2 << endl; //option 2
+	cout << opt3 << endl; //option 3
+	cout << opt4 << endl; //option 4
 }
 //function for conversion of seconds->hours->minutes
 void secToHoursMin(double seconds)
 {
-    const int time = 60; //using this as base division factor
-    double minutes;
-    double hours;
-    
-    minutes = seconds / time; //gives minutes from seconds
-    hours = (seconds / time) / time; //gives hours from seconds
-    std::cout << "there are: " << hours << " hours." << std::endl;
-    std::cout << "there are: " << minutes << " minutes." << std::endl;
-    std::cout << "there are: " << seconds << " seconds." << std::endl;
-    //displays all of the measures from seconds
+	while (cin.fail())
+	{
+		cout << "please enter a valid input: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> seconds;
+	}
+	const int time = 60; //using this as base division factor
+	double minutes;
+	double hours;
+
+	minutes = seconds / time; //gives minutes from seconds
+	hours = (seconds / time) / time; //gives hours from seconds
+	cout << "there are: " << hours << " hours." << endl;
+	cout << "there are: " << minutes << " minutes." << endl;
+	cout << "there are: " << seconds << " seconds." << endl;
+	//displays all of the measures from seconds
 }
 
 void farToCel(double convert) //conversion from far to cel
 {
-    const int num = 32;
-    convert = (convert - num) / 1.8;
-    std::cout << "that is "<< convert << " degrees Celcius." << std::endl;
-    
+	while (cin.fail())
+	{
+		cout << "please enter a valid input: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> convert;
+	}
+	const int num = 32;
+	convert = (convert - num) / 1.8;
+	cout << "that is " << convert << " degrees Celcius." << endl;
+
 }
 
 void celToFar(double revert) //conversion from cel to far
 {
-    const int num = 32;
-    revert = (revert * 1.8) + num;
-    std::cout << "that is "<< revert << " degrees Farenhiet." << std::endl;
+	while (cin.fail())
+	{
+		cout << "please enter a valid input: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> revert;
+	}
+	const int num = 32;
+	revert = (revert * 1.8) + num;
+	cout << "that is " << revert << " degrees Farenhiet." << endl;
 }
 
 int main()
 {
-printMeFirst("Mohammed Abbas"," CS116-02 Thursday – Lab1","1.1"); //calling printMeFirst 
-options(" 1--Seconds to Hours"," 2--F to C"," 3--C to F"," 4--QUIT"); //calling options
+	char menuOpt;
+	int select;
+	do {
 
-std::cout << "Please select a option from the list...\n";
-std::cout << "Only use positive integers (1 - 4): ";
-int option = 4; //max number of options
-int select;
-std::cin >> select; //user selects the option
-    while(select <= option || select == option)
-    {
-        if(select==1)
-        {
-            std::cout << "Enter Seconds to convert: ";
-            double x;
-            std::cin >> x;
-            secToHoursMin(x); //calling seconds -> hours
-            break;
-        }
-        else if(select==2)
-        {
-            std::cout << "Enter degrees Farenheit to convert: ";
-            double y;
-            std::cin >> y;
-            farToCel(y); //calls far to cel
-            break;
-        }
-        else if(select==3)
-        {
-            std::cout << "Enter degrees Celcius to convert: ";
-            double z;
-            std::cin >> z;
-            celToFar(z); //calls cel to far
-            break;
-        }
-        else if(select==4)
-        {
-            std::cout << "End...";
-            break;
-        }
-        else
-        {
-            std::cout << "Not valid selection, please enter a valid input (1 - 4): \n";
-        }
-        
-    }
-
-
+		cout << "-----------------------------------------------------------\n";
+		printMeFirst("Mohammed Abbas", " CS116-02 Thursday – Lab1", "1.1"); //calling printMeFirst 
+		options(" 1--Seconds to Hours", " 2--F to C", " 3--C to F", " 4--QUIT"); //calling options
+		cout << "Please select a option from the list...\n";
+		cout << "Only use positive integers (1 - 4): ";
+		cin >> select; //user selects the option
+		if (select == 1)
+		{
+			cout << "Enter Seconds to convert: ";
+			double x;
+			cin >> x;
+			secToHoursMin(x); //calling seconds -> hours
+		}
+		if (select == 2)
+		{
+			cout << "Enter degrees Farenheit to convert: ";
+			double y;
+			cin >> y;
+			farToCel(y); //calls far to cel
+		}
+		if (select == 3)
+		{
+			cout << "Enter degrees Celcius to convert: ";
+			double z;
+			cin >> z;
+			celToFar(z); //calls cel to far
+		}
+		if (select == 4)
+		{
+			cout << "End...";
+			break;
+		}
+		cout << "Would you like to return to the menu (y/n): ";
+		cin >> menuOpt;
+		}
+while (menuOpt == 'y');
 }
